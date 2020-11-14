d27b79c9c90f09ad61e473d0cf40b4380055201e
nhatdoan91 <36098431+nhatdoan91@users.noreply.github.com>
2020-11-14 11:17:00 -0800

    DONE

--- main.js
+++ main.js
@@ -3,6 +3,37 @@ const modal = document.querySelector('#my-modal');
 const modalBtn = document.querySelector('#modal-btn');
 const closeBtn = document.querySelector('.close');
 
+document.getElementById("dragbleButton").onclick = function() {myFunction1()};
+function myFunction1() {
+  let x =document.getElementById("dragable").getAttribute("draggable");
+  console.log(x);
+  if(x=="false")
+  {
+    document.getElementById("dragable").setAttribute("draggable","true");
+    document.getElementById("dragbleButton").style.backgroundColor = "#3876ac";
+    //console.log(x);
+  }else if(x=="true")
+  {
+    document.getElementById("dragable").setAttribute("draggable","false");
+    document.getElementById("dragbleButton").style.backgroundColor = "#e7e7e7";
+    //console.log(x);
+  }
+  //document.getElementById("dragbleButton").setAttribute("id","dragbleButtonOnClick");
+}
+//drag and drop 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
 // Events
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
