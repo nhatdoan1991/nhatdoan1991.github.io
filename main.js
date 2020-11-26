0133982d5ebd90a91f0e052b0f5a31539659c17f
nhatdoan1991 <nhatdoan@csus.edu>
2020-11-26 13:50:32 -0800

    polish order queue

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-// Get DOM Elements
-const modal = document.querySelector('#my-modal');
-const modalBtn = document.querySelector('#modal-btn');
-const closeBtn = document.querySelector('.close');
 
-document.getElementById("dragbleButton").onclick = function() {myFunction1()};
+document.getElementById("dragble-button").onclick = function() {myFunction1()};
 function myFunction1() {
-  let x =document.getElementById("dragable").getAttribute("draggable");
-  console.log(x);
-  if(x=="false")
-  {
-    document.getElementById("dragable").setAttribute("draggable","true");
-    document.getElementById("dragbleButton").style.backgroundColor = "#3876ac";
-    //console.log(x);
-  }else if(x=="true")
-  {
-    document.getElementById("dragable").setAttribute("draggable","false");
-    document.getElementById("dragbleButton").style.backgroundColor = "#e7e7e7";
-    //console.log(x);
+  var x =document.getElementsByClassName("draggable");
+  for (i = 0; i < x.length; i++) {
+    let temp = x[i].getAttribute("Draggable");
+    if(temp=="false")
+    { 
+      x[i].setAttribute("draggable","true");
+      document.getElementById("dragble-button").style.backgroundColor = "#3876ac";
+      document.getElementById("unlockIcon").className = "fa fa-unlock";
+    }else if(temp=="true")
+    {
+      x[i].setAttribute("draggable","false");
+      document.getElementById("dragble-button").style.backgroundColor = "#6e6d6a";
+      document.getElementById("unlockIcon").className = "fa fa-unlock-alt"; 
+    }
   }
   //document.getElementById("dragbleButton").setAttribute("id","dragbleButtonOnClick");
 }
@@ -36,8 +34,29 @@ function drop(ev) {
 }
 
 //drag and drop try 2
+var row;
 
+function start(){  
+  row = event.target; 
+}
+function dragover(){
+  var e = event;
+  e.preventDefault(); 
+  
+  let children= Array.from(e.target.parentNode.parentNode.children);
+  
+  if(children.indexOf(e.target.parentNode)>children.indexOf(row))
+    e.target.parentNode.after(row);
+  else
+    e.target.parentNode.before(row);
+}
+
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
 // Events
+
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
@@ -56,5 +75,29 @@ function closeModal() {
 function outsideClick(e) {
   if (e.target == modal) {
     modal.style.display = 'none';
+  }
+}
+
+function pop(r) {
+  var i = r.parentNode.parentNode.rowIndex;
+  document.getElementById("allOrderTable").deleteRow(i);
+}
+
+//dropdown
+function dropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.button-drop')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
 }
