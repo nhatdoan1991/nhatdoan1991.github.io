c0895a4905adf6aea66124f68713766996597838
nhatdoan1991 <nhatdoan@csus.edu>
2020-11-26 14:16:07 -0800

    Order Queue Polish

--- main.js
+++ main.js
@@ -101,3 +101,36 @@ window.onclick = function(event) {
     }
   }
 }
+function filterTable() {
+  var input, filter, table, tr, td, i, txtValue,txtValue2, txtValue3, txtValue4;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  //table = document.getElementById("allOrderTable");
+  tables= document.getElementsByClassName("table-content");
+  for(j = 0; j < tables.length; j++)
+  {
+    tr = tables[j].getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      td2 = tr[i].getElementsByTagName("td")[1];
+      td3 = tr[i].getElementsByTagName("td")[2];
+      td4 = tr[i].getElementsByTagName("td")[3];
+      if (td || td2 || td3) {
+        txtValue = td.textContent || td.innerText;
+        txtValue2 = td2.textContent || td2.innerText;
+        txtValue3 = td3.textContent || td3.innerText;
+        txtValue4 = td4.textContent || td4.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1 ||
+            txtValue2.toUpperCase().indexOf(filter) > -1 ||
+            txtValue3.toUpperCase().indexOf(filter) > -1 ||
+            txtValue4.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        }else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+  //tr = table.getElementsByTagName("tr");
+ 
+}
